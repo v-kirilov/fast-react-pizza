3543c0ca06ed7239b04b776a904c5b1dfeded2e0
v-kirilov <v_kirilov87@protonmail.ch>
2025-10-04 11:18:53 +0000

    Displaying a loading indicator.

---
+++ src/ui/Loader.jsx
@@ -0,0 +1,6 @@
+//This can be called spinner or loading indicator of something else cause this is confusing
+function Loader() {
+  return <div className="loader"></div>;
+}
+
+export default Loader;
