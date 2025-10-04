6998b7e13e259f058560997c02906c07f097db32
v-kirilov <v_kirilov87@protonmail.ch>
2025-10-04 08:39:45 +0000

    Building the App Layout.

---
+++ src/ui/Header.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+function Header() {
+    return (
+        <div>
+            <Link to="/">Fast React Pizza Co.</Link>
+            <p>Your one-stop shop for delicious pizza!</p>
+        </div>
+    )
+}
+
+export default Header
