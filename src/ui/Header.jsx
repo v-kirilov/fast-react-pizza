f780ad86031dde4dfceffb9ac4bb6dc3243b8ec1
v-kirilov <v_kirilov87@protonmail.ch>
2025-10-05 08:36:09 +0000

    Fetching orders.

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
 function Header() {
     return (
         <div>
             <Link to="/">Fast React Pizza Co.</Link>
+            <SearchOrder />
             <p>Your one-stop shop for delicious pizza!</p>
         </div>
     )
