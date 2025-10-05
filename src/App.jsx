2267ea25325cbbec0f033c1e27474cd1e705af4c
v-kirilov <v_kirilov87@protonmail.ch>
2025-10-05 11:10:44 +0000

    Setting up Tailwind CSS.

--- src/App.jsx
+++ src/App.jsx
@@ -48,3 +48,6 @@ function App() {
 export default App;
 
 //npm i react-router-dom@6
+// install tailwindcss
+//Install tailwind prettier plugin
+//npm install -D prettier prettier-plugin-tailwindcss
