9c30633cb2430d6383ca29006af88c68628a48aa
v-kirilov <v_kirilov87@protonmail.ch>
2025-10-05 11:20:56 +0000

    Working with color.

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -2,11 +2,11 @@ import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
 function Header() {
     return (
-        <div>
+        <header className="bg-yellow-500">
             <Link to="/">Fast React Pizza Co.</Link>
             <SearchOrder />
             <p>Your one-stop shop for delicious pizza!</p>
-        </div>
+        </header>
     )
 }
 
