4b97efd4ff41c60c202970472e17f8bf43fc5132
v-kirilov <v_kirilov87@protonmail.ch>
2025-10-11 13:27:31 +0000

    fixing the loader style.

--- src/ui/Loader.jsx
+++ src/ui/Loader.jsx
@@ -1,6 +1,10 @@
 //This can be called spinner or loading indicator of something else cause this is confusing
 function Loader() {
-  return <div className="loader"></div>;
+  return (
+    <div className="absolute inset-0 flex items-center justify-center bg-slate-200/20 backdrop-blur-sm">
+      <div className="loader"></div>;
+    </div>
+  );
 }
 
 export default Loader;
