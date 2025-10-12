c580a0b5ce44b74f387d36e21873fc2da7ce80c6
v-kirilov <v_kirilov87@protonmail.ch>
2025-10-12 08:12:36 +0000

    Styling the menu.

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,10 +1,16 @@
-import {useLoaderData} from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
 import { getMenu } from "../../services/apiRestaurant";
 import MenuItem from "./MenuItem";
 
 function Menu() {
   const menu = useLoaderData();
-  return <ul>{menu.map(pizza => <MenuItem key={pizza.id} pizza={pizza} />)}</ul>;
+  return (
+    <ul className="divide-y divide-stone-200 px-2">
+      {menu.map((pizza) => (
+        <MenuItem key={pizza.id} pizza={pizza} />
+      ))}
+    </ul>
+  );
 }
 
 export async function loader() {
