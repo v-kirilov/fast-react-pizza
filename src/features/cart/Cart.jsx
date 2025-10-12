2fcf0051b564e1fafe8315c3e8a2beb303a963ca
v-kirilov <v_kirilov87@protonmail.ch>
2025-10-12 20:20:08 +0000

    Building the cart page.

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -2,34 +2,19 @@ import LinkButton from "../../ui/LinkButton";
 import Button from "../../ui/Button";
 import CartItem from "./CartItem";
 import { useSelector } from "react-redux";
-
-const fakeCart = [
-  {
-    pizzaId: 12,
-    name: "Mediterranean",
-    quantity: 2,
-    unitPrice: 16,
-    totalPrice: 32,
-  },
-  {
-    pizzaId: 6,
-    name: "Vegetale",
-    quantity: 1,
-    unitPrice: 13,
-    totalPrice: 13,
-  },
-  {
-    pizzaId: 11,
-    name: "Spinach and Mushroom",
-    quantity: 1,
-    unitPrice: 15,
-    totalPrice: 15,
-  },
-];
+import { getCart } from "./cartSlice";
+import { useDispatch } from "react-redux";
+import { clearCart } from "./cartSlice";
+import EmptyCart from "./EmptyCart";
 
 function Cart() {
-  const cart = fakeCart;
-    const userName = useSelector((state) => state.user.userName);
+  const userName = useSelector((state) => state.user.userName);
+  const cart = useSelector(getCart);
+  const dispatch = useDispatch();
+
+  if (!cart.length) {
+    return <EmptyCart />;
+  }
 
   return (
     <div className="px-4 py-3">
@@ -37,7 +22,7 @@ function Cart() {
 
       <h2 className="mt-7 text-xl font-semibold">Your cart, {userName}</h2>
 
-      <ul className="divide-y divide-stone-200 border-b mt-3">
+      <ul className="mt-3 divide-y divide-stone-200 border-b">
         {cart.map((item) => (
           <CartItem key={item.pizzaId} item={item} />
         ))}
@@ -47,7 +32,9 @@ function Cart() {
         <Button type="primary" to="/order/new">
           Order pizzas
         </Button>
-        <Button type="secondary">Clear cart</Button>
+        <Button type="secondary" onClick={() => dispatch(clearCart())}>
+          Clear cart
+        </Button>
       </div>
     </div>
   );
